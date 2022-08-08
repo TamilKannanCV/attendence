25b52f3d03aec3c4fc5abe0bc773c547713376f1
TamilKannanCV <tamilkk100@gmail.com>
2022-08-08 22:49:00 +0530

    ui login created

---
+++ app/src/main/java/com/attendance/app/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.attendance.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.attendance.app.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
